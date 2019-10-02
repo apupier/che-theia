23c284deb4b402418a885df3cd5183085abb5844
Mykola Morhun <mmorhun@redhat.com>
2019-10-02 13:52:04 +0000

    Handle sparseCheckoutDir parameter of project source in devfile

--- plugins/factory-plugin/src/git.ts
+++ plugins/factory-plugin/src/git.ts
@@ -7,12 +7,47 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
+
+import * as path from 'path';
+import * as fs from 'fs-extra';
 import { execute } from './exec';
 
 export interface GitUpstreamBranch {
     remote: string;
     branch: string;
     remoteURL?: string;
+}
+
+export async function initRepository(baseDir: string): Promise<void> {
+    await execGit(baseDir, 'init');
+}
+
+export async function setConfig(projectPath: string, key: string, value: string): Promise<void> {
+    await execGit(projectPath, 'config', key, value);
+}
+
+export function getRemoteURL(remote: string, projectPath: string): Promise<string | undefined> {
+    return execGit(projectPath, 'config', '--get', `remote.${remote}.url`);
+}
+
+/**
+ * Performs sparse checkout.
+ * @param projectPath path to the directory where project should be located, must exist
+ * @param repositoryUri git repository location, e.g. `https://github.com/eclipse/che.git`
+ * @param sparseCheckoutDirectory directory which should be clonned, e.g. `core/che-core-api-model`
+ * @param commitReference branch or tag or commit id of the remote repository to checkout from
+ */
+export async function sparseCheckout(projectPath: string, repositoryUri: string, sparseCheckoutDirectory: string, commitReference: string): Promise<void> {
+    await initRepository(projectPath);
+    // Enable sparse checkout feature
+    await setConfig(projectPath, 'core.sparsecheckout', 'true');
+    // Write sparse checkout directory
+    const gitInfoFolderPath = path.join(projectPath, '.git/info/');
+    fs.ensureDirSync(gitInfoFolderPath);
+    fs.writeFileSync(path.join(gitInfoFolderPath, 'sparse-checkout'), sparseCheckoutDirectory);
+    // Add remote, pull changes and create the selected directory content
+    await execGit(projectPath, 'remote', 'add', '-f', 'origin', repositoryUri);
+    await execGit(projectPath, 'pull', 'origin', commitReference);
 }
 
 export async function getUpstreamBranch(projectPath: string): Promise<GitUpstreamBranch | undefined> {
@@ -25,10 +60,6 @@ export async function getUpstreamBranch(projectPath: string): Promise<GitUpstrea
         gitUpstreamBranch.remoteURL = await getRemoteURL(gitUpstreamBranch.remote, projectPath);
     }
     return gitUpstreamBranch;
-}
-
-export function getRemoteURL(remote: string, projectPath: string): Promise<string | undefined> {
-    return execGit(projectPath, 'config', '--get', `remote.${remote}.url`);
 }
 
 export function parseGitUpstreamBranch(gitBranchvvOutput: string): GitUpstreamBranch | undefined {
